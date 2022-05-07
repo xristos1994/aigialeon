bfd7a30ca33e9d3435ec2029fa0e40931368c5f0
Christos Korompokis <christos.korompokis@oddschecker.com>
2022-05-07 10:46:50 +0300

    Create our court page

---
+++ src/cms/preview-templates/OurCourtPagePreview.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { OurCourtPageTemplate } from '../../templates/our-court-page'
+
+const OurCourtPagePreview = ({ entry, widgetFor }) => (
+  <OurCourtPageTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+  />
+)
+
+OurCourtPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default OurCourtPagePreview
